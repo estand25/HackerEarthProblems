cfa60aada369e3a0086707c57aa631f656fcb5bd
Standley Eugene <estand25@gmail.com>
2017-09-06 20:58:32 -0400

    HackerEarth topics

---
+++ src/Basic_Programming/Implementation/CountNumbers.kt
@@ -0,0 +1,30 @@
+package Basic_Programming.Implementation
+
+/*
+Basic_Programming.Implementation
+Standley Eugene
+9/6/2017
+*/
+fun main(args: Array<String>){
+    var t = readLine()!!.toInt()
+    var c = readLine()!!.toInt()
+    var numberCount: Int = 0
+
+    for(aa in 1..t) {
+        var s = readLine()!!.toCharArray()
+        for (a in 0..(c-1)) {
+            var isNumber = false
+
+            if (!s[a].isLetter())
+                isNumber = true
+
+            if(s[a].isLetter() && isNumber)
+                numberCount++
+//            else
+//                isNumber = false
+            if (!s[a].isLetter())
+                println(s[a]+" "+isNumber+" "+numberCount)
+        }
+        println(numberCount)
+    }
+}
--- src/Basic_Programming/Implementation/ques2.kt
+++ src/Basic_Programming/Implementation/ques2.kt
@@ -7,19 +7,19 @@ Standley Eugene
 */
 fun main(args: Array<String>){
     var nxy = readLine()!!.replace(" ", ",").split(",").map { it.trim()}
+    var b = readLine()!!.replace(" ", ",").split(",").map{ it.trim()}
 
     var n = nxy[0].toInt()
     var x = nxy[1].toInt()
     var y = nxy[2].toInt()
     var xy : MutableList<String> = mutableListOf<String>()
 
-    for(aa in x..(y-1)) {
+    for(aa in x..y) {
         xy.add(aa.toString())
     }
 
-    for(a in 0..(y-1)){
-        var b = readLine()!!.split(",").map{ it.trim()}
-
-    }
-
+    if(b.containsAll(xy))
+        println("YES")
+    else
+        println("NO")
 }
