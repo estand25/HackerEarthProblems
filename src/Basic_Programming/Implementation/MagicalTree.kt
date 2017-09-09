4e5a76044318d2d814702fccb4b214893c121fc5
Standley Eugene <estand25@gmail.com>
2017-09-09 12:51:24 -0400

    HackerEarth topics

--- src/Basic_Programming/Implementation/MagicalTree.kt
+++ src/Basic_Programming/Implementation/MagicalTree.kt
@@ -9,38 +9,58 @@ fun main(args: Array<String>){
     var L = readLine()!!.toInt()
     var maxExpres: Int?
 
-    for(i in 0..L){
+    for(i in 0..(L-1)){
         var input = readLine()!!.toCharArray()
-        var temp: Int?
-        var temp1 : MutableList<String> = mutableListOf<String>()
+        var temp: Int = 0
+        var temp1 : MutableList<Int> = mutableListOf<Int>()
         var temp2 : MutableList<String> = mutableListOf<String>()
 
         for (j in 0..(input.size-1)){
-            if(j%2 == 0) {
-                temp1.add(input[j].toString())
+            if(temp == 0)
+                if(input[j].isLetterOrDigit())
+                    temp1.add(input[j].toString().toInt())
+                else
+                    temp2.add(input[j].toString())
+        }
 
-                if((temp2.size-1)%2 == 0){
-                    println("m : " + if(temp1[j].isNullOrEmpty()) "null" else temp1[j])
-                    //println("b : " + if(temp2[j].isNullOrEmpty()) "null" else temp2[j])
+//        for(k in 0..(temp1.size-1))
+//            print(temp1[k].toString())
+//
+//        println("")
+//        println(temp1.size)
+//        println("")
+//
+//        for(m in 0..(temp2.size-1))
+//            print(temp2[m].toString())
+//
+//        println("")
+//        println(temp2.size)
+//        println("")
 
-                    println("\n")
+        for(n in 1..(temp1.size-1)){
+            if(n == 1)
+                if(temp2[0].toString() == "+") {
+                    temp = temp1[0] + temp1[1]
+
+//                    println(temp.toString() + " = " + temp1[0].toString().toInt() + " + " + temp1[1].toString().toInt())
                 }
-            }
-            else {
-                temp2.add(input[j].toString())
-            }
+                else {
+                    temp = temp1[0] - temp1[1]
+//                    println(temp.toString() + " = " + temp1[0].toString().toInt() + " - " + temp1[1].toString().toInt())
+                }
+            else
+                if(temp2[n-1].toString() == "+") {
+                    temp += temp1[n]
+//                    println(temp.toString() + " += " + temp1[n].toString().toInt())
+                }
+                else {
+                    temp -= temp1[n]
+//                    println(temp.toString() + " -= " + temp1[n].toString().toInt())
+                }
         }
 
-        println("\n")
-
-        for(k in 0..(temp1.size-1)){
-            print(temp1[k]+" ")
-        }
-
-        println("\n")
-
-        for(i in 0..(temp2.size-1)){
-            print(" " + temp2[i])
-        }
+        println("")
+        println(temp)
+        println("")
     }
 }
