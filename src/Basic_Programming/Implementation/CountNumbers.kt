2ee1fdd2ea52ffd64dca0d4591b4902650c1a1ce
Standley Eugene <estand25@gmail.com>
2017-09-09 07:43:52 -0400

    HackerEarth topics

--- src/Basic_Programming/Implementation/CountNumbers.kt
+++ src/Basic_Programming/Implementation/CountNumbers.kt
@@ -10,9 +10,14 @@ fun main(args: Array<String>){
     var c = readLine()!!.toInt()
     var numberCount: Int = 0
     var xy : MutableList<Boolean> = mutableListOf<Boolean>()
+    var num : MutableList<Int> = mutableListOf<Int>()
+    var num2 : MutableList<Int> = mutableListOf<Int>()
 
     for(aa in 1..t) {
         var s = readLine()!!.toCharArray()
+        var t = s
+        var prevIsNumber = false
+
         for (a in 0..(c-1)) {
             var isNumber = false
 
@@ -21,17 +26,22 @@ fun main(args: Array<String>){
 
             if(s[a].isLetter() && isNumber)
                 numberCount++
-//            else
-//                isNumber = false
-            //if (!s[a].isLetter()) {
-            println(s[a] + " " + isNumber + " " + numberCount)
 
-            xy.add(isNumber)
+            if(isNumber)
+                numberCount =  1
+            else
+                numberCount = 0
 
-            //if(xy.contains(false))
+            if(a >= 1) {
+                println(s[a] + " " + isNumber + " " + numberCount)
 
-            //}
+                xy.add(isNumber)
+                num.add(numberCount)
+            }
         }
-        println(numberCount)
+        for(bb in 1..(num.size-1)){
+            if(num[bb] > 0)
+                 num2.add(num[bb])
+        }
     }
 }
