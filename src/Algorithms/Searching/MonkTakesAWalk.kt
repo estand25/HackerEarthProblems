849bf7c242d3bb75a1659364b2c1798d6a0b3218
Standley Eugene <estand25@gmail.com>
2017-09-23 19:05:08 -0400

    HackerEarth topics

--- src/Algorithms/Searching/MonkTakesAWalk.kt
+++ src/Algorithms/Searching/MonkTakesAWalk.kt
@@ -12,8 +12,46 @@ fun main(args: Array<String>){
     var vowel = arrayOf("A","E","I","O","U","a","e","i","o","u")
 
     for(a in 0..(T-1)){
-        var input = readLine()!!.toCharArray()
+        var input = readLine()!!.toString()
         var count = 0
+        var inputv2 = input
+
+        for(b in 0..(vowel.size-1)){
+            var oLength = inputv2.length
+            var replaced = inputv2.replace(vowel[b],"").length
+
+            count += oLength-replaced
+        }
+        println(count)
+    }
+}
+//        println(vowel.size-1)
+//println(count)
+//        var done = false
+//            println(replaced)
+//            println(nfilter)
+//            println(nfilterCount)
+//println(vowel[b])
+//println(inputv2)
+//println(inputv2.filter { it -> it.equals("1") }.length)
+//done = false
+
+//            while(input.contains(vowel[b])) {
+//                println("match")
+//                count++
+//                if (input.contains(vowel[b])) {
+//                    println("match")
+//                    count++
+//                    //done = false
+//                }
+//                else{
+//                    println("Un-match")
+//                    done = true
+//                }
+//                println(vowel[b])
+//            }
+//        var input = readLine()!!.toCharArray()
+//        var count = 0
 //        var done = false
 //        var loc = 1
 
@@ -27,11 +65,9 @@ fun main(args: Array<String>){
 //            }
 //        }
 
-        for (b in 0..(vowel.size-1)){
-            println(vowel[b])
-            count += input.filter { it -> it.equals(vowel[b]) }.size
-        }
-
-        println(count)
-    }
-}
+//        for (b in 0..(vowel.size-1)){
+//            println(vowel[b])
+//            count += input.filter { it -> it.equals(vowel[b]) }.size
+//        }
+//
+//        println(count)
