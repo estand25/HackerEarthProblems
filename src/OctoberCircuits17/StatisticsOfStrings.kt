014782bf2b287d73d31a3a30bf0dc7ffd81a5240
Standley Eugene <estand25@gmail.com>
2017-10-28 17:00:43 -0400

    HackerEarth topics

--- src/OctoberCircuits17/StatisticsOfStrings.kt
+++ src/OctoberCircuits17/StatisticsOfStrings.kt
@@ -1,5 +1,7 @@
 package OctoberCircuits17
 
+import java.math.BigInteger
+
 /*
 OctoberCircuits17
 Standley Eugene
@@ -7,5 +9,30 @@ Standley Eugene
 */
 
 fun main(args: Array<String>){
+    var n = readLine()!!.toInt()
 
+    for(a in 0..(n-1)) {
+        var a = readLine()!!.replace(" ", ",").split(",").map { it.trim() }
+
+        var superBig = 1
+        var nSuperBig = -1
+
+        var result: String? = null
+
+        if ((BigInteger.valueOf(superBig.toLong()) - BigInteger.valueOf(a[0].toLong())).toInt() > 0)
+            result = "1"
+        else if ((BigInteger.valueOf(superBig.toLong()) - BigInteger.valueOf(a[0].toLong())).toInt() == 0)
+            result = "0"
+        else
+            result = "-1"
+
+        if ((BigInteger.valueOf(nSuperBig.toLong()) - BigInteger.valueOf(a[1].toLong())).toInt() > 0)
+            result += " 1"
+        else if ((BigInteger.valueOf(nSuperBig.toLong()) - BigInteger.valueOf(a[1].toLong())).toInt() == 0)
+            result += " 0"
+        else
+            result += " -1"
+
+        println(result)
+    }
 }
