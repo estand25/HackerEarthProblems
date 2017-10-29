cb4a7750d46f8578a38c43402dbc5971937fd8e3
Standley Eugene <estand25@gmail.com>
2017-10-29 09:50:59 -0400

    HackerEarth topics

--- src/OctoberCircuits17/CardGame.kt
+++ src/OctoberCircuits17/CardGame.kt
@@ -6,51 +6,51 @@ Standley Eugene
 10/22/2017
 */
 fun main(args: Array<String>){
-    var n = readLine()!!.toInt()
-    var nCards = readLine()!!.replace(" ",",").split(",").map { it.trim() }
+    var n = readLine()!!.toLong()
+    var nCards = readLine()!!.replace(" ",",").split(",").map { it.trim().toLong() }
 
-    var m = readLine()!!.toInt()
-    var mCards = readLine()!!.replace(" ", ",").split(",").map { it.trim() }
+    var m = readLine()!!.toLong()
+    var mCards = readLine()!!.replace(" ", ",").split(",").map { it.trim().toLong() }
 
-    var nCardList : MutableList<String> = nCards.toMutableList()
-    var mCardList : MutableList<String> = mCards.toMutableList()
+    var nCardList : MutableList<Long> = nCards.toMutableList()
+    var mCardList : MutableList<Long> = mCards.toMutableList()
 
     for(a in 0..(n-1)){
         for(b in 0..(m-1)) {
-            if(nCardList[a].toInt() <= mCardList[b].toInt())
-                nCardList[a] = (nCardList[a].toInt()+1).toString()
+            if(nCardList[a.toInt()].toInt() <= mCardList[b.toInt()].toInt())
+                nCardList[a.toInt()] = (nCardList[a.toInt()].toInt()+1).toLong()
         }
     }
 
     var nChanges : MutableList<String>  = mutableListOf<String>()
 
     for(c in 0..(nCardList.size-1)) {
-        if(nCardList.get(c) != nCards.get(c))
+        if(nCardList[c] != nCards[c])
             nChanges.add("C")
         else
-            nChanges.add(nCardList[c])
+            nChanges.add(nCardList[c].toString())
     }
 
     var nChangeItem : MutableList<String>  = mutableListOf<String>()
     var nChange2 : MutableList<String>  = nChanges.toMutableList()
 
-    for(c in 0..(nChanges.map {a -> a.equals("C") }.size-1)) {
-       if(nChanges.map {a -> a.equals("C") }[c])
-           nChangeItem.add(nCardList[c])
+    for(d in 0..(nChanges.map {a -> a == "C" }.size-1)) {
+       if(nChanges.map {a -> a == "C" }[d])
+           nChangeItem.add(nCardList[d].toString())
     }
 
     nChangeItem.max()
 
-    for(c in 0..(nChanges.map {a -> a.equals("C") }.size-1)) {
-        if(nChanges.map {a -> a.equals("C") }[c])
-            nChanges[c] = nChangeItem.max().toString()
+    for(e in 0..(nChanges.map {a -> a == "C" }.size-1)) {
+        if(nChanges.map {a -> a == "C" }[e])
+            nChanges[e] = nChangeItem.max().toString()
     }
 
     var dollar: Int = 0
 
-    for(c in 0..(nChange2.size-1)){
-        if (nChange2.map {a -> a.equals("C") }[c]) {
-            dollar=dollar+nChanges[c].toInt()-nCards[c].toInt()
+    for(f in 0..(nChange2.size-1)){
+        if (nChange2.map {a -> a == "C" }[f]) {
+            dollar=dollar+nChanges[f].toInt()-nCards[f].toInt()
         }
     }
 
