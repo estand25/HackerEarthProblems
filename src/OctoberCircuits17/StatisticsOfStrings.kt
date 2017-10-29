5e6cf22bec82f05dd48441e0804dcbc024e44b74
Standley Eugene <estand25@gmail.com>
2017-10-29 09:21:25 -0400

    HackerEarth topics

--- src/OctoberCircuits17/StatisticsOfStrings.kt
+++ src/OctoberCircuits17/StatisticsOfStrings.kt
@@ -10,31 +10,28 @@ Standley Eugene
 fun main(args: Array<String>){
     var n = BigInteger.valueOf(readLine()!!.toLong())
 
-    //BigInteger.valueOf(n)
-//    for(a in 0..(n-1)) {
-        var a = readLine()!!.replace(" ", ",").split(",").map { it.trim() }
+    var a = readLine()!!.replace(" ", ",").split(",").map { it.trim() }
 
-        var superBig = 1
-        var nSuperBig = -1
+    var superBig = 1
+    var nSuperBig = -1
 
-        var result: String? = null
+    var result: String? = null
 
-        if (superBig - (a[0].toLong()).toInt() > 0)
-            result = "1"
-        else if (superBig - (a[0].toLong()).toInt() == 0)
-            result = "0"
-        else
-            result = "-1"
+    if (superBig - (a[0].toLong()).toInt() > 0)
+        result = "1"
+    else if (superBig - (a[0].toLong()).toInt() == 0)
+        result = "0"
+    else
+        result = "-1"
 
-        result += " "
+    result += " "
 
-        if (nSuperBig - (a[0].toLong()).toInt() > 0)
-            result += "1"
-        else if (nSuperBig - (a[0].toLong()).toInt() == 0)
-            result += "0"
-        else
-            result += "-1"
+    if (nSuperBig - (-a[0].toLong()).toInt() > 0)
+        result += "1"
+    else if (nSuperBig - (-a[0].toLong()).toInt() == 0)
+        result += "0"
+    else
+        result += "-1"
 
-        println(result)
-//    }
+    println(result)
 }
