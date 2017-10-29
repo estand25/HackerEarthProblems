9d91c42ab5a08ba3c444dc9b95f691e9d1009157
Standley Eugene <estand25@gmail.com>
2017-10-29 09:52:06 -0400

    HackerEarth topics

--- src/OctoberCircuits17/PolynomialSign.kt
+++ src/OctoberCircuits17/PolynomialSign.kt
@@ -1,10 +1,37 @@
 package OctoberCircuits17
 
+import java.math.BigInteger
+
 /*
 OctoberCircuits17
 Standley Eugene
 10/27/2017
 */
 fun main(args: Array<String>){
+    var n = BigInteger.valueOf(readLine()!!.toLong())
 
+    var a = readLine()!!.replace(" ", ",").split(",").map { it.trim() }
+
+    var superBig = 1
+    var nSuperBig = -1
+
+    var result: String? = null
+
+    if (superBig - (a[0].toLong()).toInt() > 0)
+        result = "1"
+    else if (superBig - (a[0].toLong()).toInt() == 0)
+        result = "0"
+    else
+        result = "-1"
+
+    result += " "
+
+    if (nSuperBig - (-a[0].toLong()).toInt() > 0)
+        result += "1"
+    else if (nSuperBig - (-a[0].toLong()).toInt() == 0)
+        result += "0"
+    else
+        result += "-1"
+
+    println(result)
 }
