3dcab31704834cf769fd0f35dc1fe3040ebc727d
Standley Eugene <estand25@gmail.com>
2017-10-29 13:46:14 -0400

    HackerEarth topics

---
+++ src/OctoberCircuits17/Replace.kt
@@ -0,0 +1,12 @@
+package OctoberCircuits17
+
+/*
+OctoberCircuits17
+Standley Eugene
+10/29/2017
+*/
+fun main (args: Array<String>){
+    var fLine = readLine()!!.replace(" ", ",").split(",").map { it.trim().toLong() }
+
+
+}
