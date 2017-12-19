64a85f157ac655f9ba6185f58435188749c335a2
Standley Eugene <estand25@gmail.com>
2017-12-18 19:46:00 -0500

    HackerEarth topics

---
+++ src/DecemberCircuit17/TwoArrays.kt
@@ -0,0 +1,22 @@
+package DecemberCircuit17
+
+fun main(args: Array<String>){
+    var NMK = readLine()!!.replace(" ",",").split(",").map { it.toInt() }
+
+    var N = NMK[0]
+    var M = NMK[1]
+    var K = NMK[2]
+
+    println("Input: N= $N, M= $M, K= $K")
+
+    var a = readLine()!!.replace(" ", ",").split(",").map{ it.toInt()}
+    var b = readLine()!!.replace(" ", ",").split(",").map{ it.toInt()}
+
+    for (i in 0..(N-1)){
+        var loop = true
+
+        while (loop){
+            var pairs:
+        }
+    }
+}
